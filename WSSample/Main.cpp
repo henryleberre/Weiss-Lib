98b0f963ba7bca6d45b4d96ca525ed7008c69d0b
Henry <24208000+henryleberre@users.noreply.github.com>
2020-06-25 17:25:33 +0200

    Now Linking To X11

--- WSSample/Main.cpp
+++ WSSample/Main.cpp
@@ -1,7 +1,14 @@
 #include <WSCore/WSInclude.h>
 
 int WS::EntryPoint(int argc, char** argv) {
-    WS::Image test("Branding/logo.png");
+    WS::Window window("Weiss Test", 1920 / 2, 720);
+    //WS::Image test("Branding/icon64x64.png");
+
+    while (true) {
+        window.Update();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
 
     std::cin.get();
     return 0;
